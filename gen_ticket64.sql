829209544bef8e7a83e67f0e4aa08c8627a7f781
Yasuyuki ENDO <eyasuyuki@gmail.com>
2020-10-22 21:29:07 +0900

    add last_id to tb_tickets

--- gen_ticket64.sql
+++ gen_ticket64.sql
@@ -1,19 +1,22 @@
 DELIMITER $$
 CREATE FUNCTION `gen_ticket64`() RETURNS BIGINT(20)
 BEGIN
-	DECLARE epoch BIGINT(20);
+    DECLARE epoch BIGINT(20);
     DECLARE current_ms BIGINT(20);
     DECLARE node INTEGER;
     DECLARE incr BIGINT(20);
+    DECLARE next_id BIGINT(20);
     
     SET node = @@server_id;
-    SET current_ms = round(CURRENT_TIMESTAMP(CURTIME(4)) * 1000);
-    SET epoch = 1459440000000; #//change your epoch
+    SET current_ms = round(UNIX_TIMESTAMP(CURRENT_TIMESTAMP(4)) * 1000);
+    SET epoch = 1600000000000;
     
     REPLACE INTO tb_tickets (stub) VALUES ('a');
-	SELECT LAST_INSERT_ID() INTO incr;
+        SELECT LAST_INSERT_ID() INTO incr;
+
+    SET next_id = (current_ms - epoch) << 22 | (node << 12) | (incr % 4096);
+    UPDATE tb_tickets SET last_id = next_id WHERE id = incr;
     
-    
-RETURN (current_ms - epoch) << 22 | (node << 12) | (incr % 4096);
+RETURN next_id;
 END$$
 DELIMITER ;
--- tb_tickets.sql
+++ tb_tickets.sql
@@ -1,6 +1,7 @@
 CREATE TABLE `tb_tickets` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `stub` char(1) NOT NULL DEFAULT '',
+  `last_id` bigint(20) unsigned NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`),
   UNIQUE KEY `stub` (`stub`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
